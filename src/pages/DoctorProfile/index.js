ff93dd398a647dc2fc007bff6519104a79c4ad63
Afrizal <afrizalbagas.03@gmail.com>
2022-04-09 11:49:12 +0700

    add verified, handle undifined photo

--- src/pages/DoctorProfile/index.js
+++ src/pages/DoctorProfile/index.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {ILNullProfile} from '../../assets';
 import {Button, Header, MainProfile, ProfileItem} from '../../component';
 
 const DoctorProfile = ({navigation, route}) => {
+  // parsing data
   const dataDoctor = route.params;
+  // console.log('data parsing: ', dataDoctor);
   return (
     <View style={styles.page}>
       <Header title="Doctor Profile" onPress={() => navigation.goBack()} />
-      <MainProfile
-        name={dataDoctor.data.fullName}
-        desc={dataDoctor.data.profession}
-        photo={{uri: dataDoctor.data.photo}}
-      />
-      <ProfileItem label="Alumnus" desc={dataDoctor.data.university} />
-      <ProfileItem
-        label="Tempat Praktik"
-        desc={dataDoctor.data.hospital_address}
-      />
-      <ProfileItem label="No. STR" desc={dataDoctor.data.str_number} />
-      <View style={styles.wrapper}>
-        <Button
-          title="Start Consultation"
-          onPress={() => navigation.navigate('Chatting', dataDoctor)}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <MainProfile
+          name={dataDoctor.data.fullName}
+          desc={dataDoctor.data.profession}
+          photo={
+            dataDoctor.data.photo === undefined
+              ? ILNullProfile
+              : {uri: dataDoctor.data.photo}
+          }
+          verified={
+            dataDoctor.data.verified === true
+              ? 'Verified ☑️'
+              : 'Not Verified ❎'
+          }
         />
-      </View>
+        <ProfileItem label="Alumnus" desc={dataDoctor.data.university} />
+        <ProfileItem
+          label="Tempat Praktik"
+          desc={dataDoctor.data.hospital_address}
+        />
+        <ProfileItem label="No. STR" desc={dataDoctor.data.str_number} />
+        <View style={styles.wrapper}>
+          {dataDoctor.data.verified === true ? (
+            <Button
+              title="Start Consultation"
+              onPress={() => navigation.navigate('Chatting', dataDoctor)}
+            />
+          ) : (
+            <Button
+              title="Start Consultation"
+              disable
+              onPress={() => navigation.navigate('Chatting', dataDoctor)}
+            />
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
