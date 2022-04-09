2480dec8f4b04bd6c6627c183d43c40ee344cae6
Afrizal <afrizalbagas.03@gmail.com>
2022-04-09 12:00:13 +0700

    change tab navigator name

--- src/router/index.js
+++ src/router/index.js
@@ -25,9 +25,9 @@ const Tab = createBottomTabNavigator();
 const MainApp = () => {
   return (
     <Tab.Navigator tabBar={(props) => <BottomNavigator {...props} />}>
-      <Tab.Screen name="Doctor" component={Doctor} />
+      <Tab.Screen name="Home" component={Doctor} />
       <Tab.Screen name="Messages" component={Messages} />
-      <Tab.Screen name="Hospitals" component={Hospitals} />
+      <Tab.Screen name="Hospital" component={Hospitals} />
     </Tab.Navigator>
   );
 };
